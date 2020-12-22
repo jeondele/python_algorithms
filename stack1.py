f3b0bd65ac24494062522b48cfac98a45c5ad1e9
jeondele <neosanglokue@naver.com>
2020-12-22 15:34:00 +0000

    프로그래머스 스택 1번. 주식가격 : 스택을 이용한 풀이

--- stack1.py
+++ stack1.py
@@ -31,3 +31,25 @@ def solution2(prices):
               answer[idx] = i-idx
               break      
   return answer
+
+
+
+
+#list[0 : 8 : 2 ] 시작 : 끝 : 간격  
+#창의적인 풀이
+#대박풀이
+def solution3(p):
+    ans = [0] * len(p)
+    stack = [0]
+    for i in range(1, len(p)):
+        if p[i] < p[stack[-1]]:
+            for j in stack[::-1]:
+                if p[i] < p[j]:
+                    ans[j] = i-j
+                    stack.remove(j)
+                else:
+                    break
+        stack.append(i)
+    for i in range(0, len(stack)-1):
+        ans[stack[i]] = len(p) - stack[i] - 1
+    return ans
