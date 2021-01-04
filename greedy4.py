c9dccf6ff31b958972d79ac1c66cd6b3234e1ff8
James Jeon <neosanglokue@naver.com>
2021-01-04 13:51:09 +0000

    프로그래머스 그리디 2번. 조이스틱(미결)

--- greedy4.py
+++ greedy4.py
@@ -1,4 +1,6 @@
-import math
+#프로그래머스 그리디 2번. 조이스틱 
+#다시풀어봐야함
+
 def solution(name):
     list =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     
@@ -6,11 +8,10 @@ def solution(name):
     for i in range(len(name)) :
         if name[i] == 'A' :
             continue
-        answer += min ( list.index(name[i]) - math.abs(list.index(name[i])- len(list)))
+        answer += min ( list.index(name[i]) , abs(list.index(name[i])- len(list)))
+        
     skip_idx = 0
     for i in range(len(name)) :
-        if i == 0 :
-            continue
         if name[i] == 'A' :
             continue
         answer += min(skip_idx+1 , i-skip_idx)
