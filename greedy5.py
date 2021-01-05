75dde8b550edf10145a8972e9e1546ae0fa813d9
James Jeon <neosanglokue@naver.com>
2021-01-05 13:49:54 +0000

    프로그래머스 그리디 3번. 큰 수 만들기 베스트 풀이 추가

--- greedy5.py
+++ greedy5.py
@@ -3,6 +3,17 @@
 from itertools import combinations
 number = '4177252841'
 k = 4
+#너무도 깔끔한 풀이.. 언제쯤 이렇게 짜지?
+def solution_best(number, k):
+    stack = []
+    for num in number :
+        while stack and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
 
 def solution_final(number, k):
     answer = []
