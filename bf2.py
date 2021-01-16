15d024c3f324b914cc53e2f2cfe4f9dd70707550
James Jeon <neosanglokue@naver.com>
2021-01-16 09:59:28 +0000

    프로그래머스 완전탐색 2번. 소수찾기

--- bf2.py
+++ bf2.py
@@ -5,6 +5,7 @@ from itertools import permutations
 def solution(numbers):
     answer = 0
     set_a = set()
+    #for 문 안쓰고 리스트로 감싸면 바로 만들수 있음
     #nl = [i for i in numbers]
     nl = list(numbers)
     for i in range(1, len(nl)+1) :
@@ -13,11 +14,18 @@ def solution(numbers):
         if int(i) <= 1 :
             continue
         cnt = 0    
-        for s in range(2, int(i)+1) :
+        #for s in range(2, int(i)+1) :
+        #    if int(i) % s == 0:
+        #        cnt += 1
+        #    if cnt > 1 :
+        #        break    
+        #if cnt == 1:
+        #    answer += 1
+        #루트한 값만 for문을 돌리면 시간복잡도를 줄일 수 있음
+        for s in range(2, int(int(i)**0.5) +1) :
             if int(i) % s == 0:
                 cnt += 1
-            if cnt > 1 :
                 break    
-        if cnt == 1:
+        if cnt == 0:
             answer += 1
     return answer
