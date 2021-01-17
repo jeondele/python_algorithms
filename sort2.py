14d77dc02bcff08cf1a28cd1f858ea0577b90fe3
James Jeon <neosanglokue@naver.com>
2021-01-17 13:32:17 +0000

    프로그래머스 정렬 2번. 가장 큰 수

--- sort2.py
+++ sort2.py
@@ -1,5 +1,6 @@
 #프로그래머스 정렬 2번. 가장 큰 수
 #ceil 할때는 math 임포트해야함
+import math
 def solution(numbers):
     def func(x) :
         return (str(x) * math.ceil(4 / len(str(x))))[0:5]
@@ -9,3 +10,5 @@ def solution_best(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=lambda x: x*3, reverse=True)
     return str(int(''.join(numbers)))
+
+print(solution([3, 30, 34, 5, 9]))
