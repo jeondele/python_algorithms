245e929dc27ceaf281eedf7468d86ffd00c81384
James Jeon <neosanglokue@naver.com>
2021-01-20 14:47:37 +0000

    예제 5. 병사 배치하기 추가

--- dp1.py
+++ dp1.py
@@ -92,3 +92,20 @@ else :
   print(d[m])
 
 #예제 4. 금광문제는 따로 풀어보기  
+
+#예제 5. 병사 배치하기
+#가장 긴 증가하는 부분 수열 LIS 로 알려진 다이나믹 프로그래밍
+#D[i] = array[i]
+#모든 원소 0 <= j < i에 대하여, D[i] = max(D[i] , D[j] +1) if array[j] < array[i]
+n = int(input())
+array = list(map(int, input().split()))
+# 순서를 뒤집어 '최장 증가 부분 수열' 문제로 변환
+array.reverse()
+# 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
+dp = [1] *n 
+
+for i in range(1,n) :
+    for j in range(0, i) :
+        if array[j] < array[i] :
+            dp[i] = max(dp[i], dp[j]+1)
+print(n- max(dp))            
