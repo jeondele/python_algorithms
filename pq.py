a59ce8a0f5571bb8e5a3f0ff8d474f3aa01cea51
James Jeon <neosanglokue@naver.com>
2021-01-23 12:31:50 +0000

    우선순위 큐 heapq 구현

--- pq.py
+++ pq.py
@@ -12,3 +12,24 @@
 #Min-Heapify()
 #힙에서 원소가 제거될때 가장 높은 노드의 값으로 대체
 
+import sys
+import heapq
+input = sys.stdin.readline
+
+def hehapsort(iterable) :
+    h = []
+    result = []
+    for value in iterable :
+        heapq.heappush(h, value)
+    for i in range(len(h)) :
+        result.append(heapq.heappop(h))
+    return result
+
+n = int(input())
+arr= []
+
+for i in range(n) :
+    arr.append(int(input()))
+res = heapsort(arr)
+for i in range(n) :
+    print(res[i])
---
+++ pq1.py
@@ -0,0 +1,16 @@
+#프로그래머스 우선순위큐 1번. 더 맵게
+import heapq
+
+def solution(scoville, K):
+    heap =[]
+    for num in scoville :
+        heapq.heappush(heap, num)
+    print(heap)
+    answer = 0
+    while heap[0] < K :
+        try :
+            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap)*2))
+        except IndexError :
+            return -1
+        answer += 1
+    return answer
